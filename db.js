23bf1f400aa551b4d0d5fbff88c8cf27dac30aa6
yash-reddy01 <yashreddy.official@gmail.com>
2024-05-08 17:12:39 +0530

    Create post schema

--- db.js
+++ db.js
@@ -10,14 +10,59 @@ mongoose.connect("mongodb+srv://admin:Yash%402001@cluster0.rkdvdjf.mongodb.net/U
 
 
 const userSchema = mongoose.Schema({
-    "username": String,
-    "password": String,
-    "email": String
+    username: String,
+    password: String,
+    email: String
+});
+
+const postSchema = mongoose.Schema({
+    user: {
+        type : mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    },
+    likes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }
+    ],
+    comments: [
+        {
+            user: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User',
+                required: true
+            },
+            text: {
+                type: String,
+                required: true
+            },
+            createdAt: {
+                type: Date,
+                default: Date.now
+            }
+        }
+    ]
 });
 
 
+const User = mongoose.model('User', userSchema);
+const Post = mongoose.model('Post', postSchema);
 
-const User = mongoose.model('User', userSchema)
-
-module.exports = User
+module.exports = {
+    User, Post
+}
 
